2642cd725657170a4dfe4d95928897600b3b4b5d
esteves001 <vesteves@itba.edu.ar>
2026-03-20 13:49:35 -0300

    Sume los cambios realizados a common.h, que incluyen la adición de campos para movimientos válidos e inválidos, así como la corrección de nombres de semáforos y mutexes para mejorar la claridad del código. Estos cambios son esenciales para el correcto funcionamiento del sistema de sincronización entre el máster, los jugadores y la vista.

--- common.h
+++ common.h
@@ -6,12 +6,40 @@
 #include <sys/types.h>
 
 typedef struct {
+    char name[16];                    // Nombre del jugador
+    unsigned int score;               // Puntaje
+    unsigned int invalid_mov;         // Solicitudes de movimientos inválidas
+    unsigned int valid_mov;           // Solicitudes de movimientos válidas
+    unsigned short x, y;              // Coordenadas en el tablero
+    pid_t pid;                        // Identificador de proceso
+    bool blocked;                     // Si el jugador está bloqueado
 } Jugador;
 
 typedef struct {
-} EstadoJuego;
+    unsigned short width;              // Ancho del tablero
+    unsigned short height;             // Alto del tablero
+    unsigned char numPlayers;          // Cantidad de jugadores
+    Jugador players[9];                // Lista de jugadores
+    bool finished;                     // Si el juego se ha terminado
+    char *board;                       // Puntero al inicio del tablero
+} GameState;
 
 typedef struct {
+    sem_t canPrint;                   // Máster → Vista: hay cambios por imprimir
+    sem_t completedPrint;             // Vista → Máster: terminó de imprimir
+    // La sincronización entre el máster y la vista involucra los semáforos puedeImprimir y impresionCompleta siguiendo un patrón
+    // simple de señalización bidireccional.
+
+    sem_t mutexWriter;               // Mutex: previene inanición del máster
+    sem_t mutexStatus;               // Mutex: protege el estado del juego
+    sem_t mutexReaders;              // Mutex: protege el contador de lectores
+    unsigned int playersReading;     // Cantidad de jugadores leyendo el estado
+    // La sincronización entre el máster y los jugadores involucra los
+    // semáforos C, D y E y la variable F
+
+    sem_t allowed_Mov[9];      // Semáforo individual para cada jugador
+    // Cada jugador, deberá esperar a que el máster procese su movimiento para poder
+    // enviar otro, lo cual se coordina mediante un semáforo único para cada jugador en el arreglo G.
 } Sincronizacion;
 
 #endif
